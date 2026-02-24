cf00617dc058a619c2b9f7700cbbfd21ece87e1c
Paulo Victor Peres Reis <82324291@ulife.com.br>
2026-02-24 20:19:49 -0300

    teste

---
+++ java.js
@@ -0,0 +1,10 @@
+let Musicas = {
+    Tipo: ['Jazz','Trap','Rock'],
+    Duracao: ['1,80','1,90','2,00'],
+    Volume: ['Baixo','Medio','Alto'],
+};
+
+console.log('Qual musica que vc gosta?', Musicas)
+console.log('Qual tipo de musica quer?', Musicas.Tipo[0])
+console.log('Voce que qual duração?', Musicas.Duracao[1])
+console.log('Qual Volume Quer?', Musicas.Volume[0])
